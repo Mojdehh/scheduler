a7290de2994a5793013dcafc0f596c7210c824fb
Mojdeh <mojdeh@Mojdehs-MacBook-Pro.local>
2021-12-07 20:29:14 -0500

    fix idention(tab size=2)

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,24 +3,25 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   const { children, confirm, danger, onClick, disabled} = props;
-   let buttonClass = "button";
+  const { children, confirm, danger, onClick, disabled} = props;
+  let buttonClass = "button";
 
-   if (confirm) {
-      buttonClass += " button--confirm";
-   }
+  if (confirm) {
+    buttonClass += " button--confirm";
+  }
 
-   if (danger) {
-      buttonClass += " button--danger";
-   }
+  if (danger) {
+    buttonClass += " button--danger";
+  }
 
-   return (
-      <button 
-         className={buttonClass}
-         onClick={onClick}
-         disabled={disabled}
-      > 
-         {children}
-      </button>
-   );
+  return (
+    <button 
+      className={buttonClass}
+      onClick={onClick}
+      disabled={disabled}
+    > 
+      {children}
+    </button>
+  );
 }
+  
