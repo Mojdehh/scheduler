aedf614c257cd853928c4d65f0377635146638e3
Mojdeh <mojdeh@Mojdehs-MacBook-Pro.local>
2021-12-07 19:01:12 -0500

    build a button with storybook

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,24 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+   const { children, confirm, danger, onClick, disabled} = props;
+   let buttonClass = "button";
+
+   if (confirm) {
+      buttonClass += " button--confirm";
+   }
+
+   if (danger) {
+      buttonClass += " button--danger";
+   }
+
+   return (
+      <button 
+         className={buttonClass}
+         onClick={onClick}
+         disabled={disabled}
+      > 
+         {children}
+      </button>
+   );
 }
