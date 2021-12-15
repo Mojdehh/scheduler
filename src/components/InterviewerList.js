6aac84a6db1a688ea9bc95bc555968475a1aab86
Mojdeh <mojdeh@Mojdehs-MacBook-Pro.local>
2021-12-14 23:21:23 -0500

    import PropTypes and validate the interviewers prop

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "components/InterviewerList.scss"
 import InterviewerListItem from "./InterviewerListItem";
+import PropTypes from 'prop-types';
 
 export default function InterviewerList(props) {
 
@@ -25,4 +26,8 @@ const interviewerListItem = props.interviewers.map((interviewer) => {
     </section>
   );
   
-};
+}
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+}
