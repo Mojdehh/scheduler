3f013e23640a786ee74d40195a3df09e2a1c5ec1
Mojdeh <mojdeh@Mojdehs-MacBook-Pro.local>
2021-12-15 22:30:14 -0500

    add data-testid attribute to dayListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -21,6 +21,7 @@ export default function DayListItem(props) {
     <li 
     className={dayClass}
     onClick={() => setDay(name)}
+    date-testid="day"
     > 
       <h2 className="text--regular">{name}</h2>
       <h3 className="text--light">{formatSpots(spots)}</h3>
