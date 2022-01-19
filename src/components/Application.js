24428c478c4e37bbc87089878f08394a82cd336f
Mojdeh <mojdeh.sbzgh@gmail.com>
2022-01-18 22:56:03 -0500

    nothing to commit

--- src/components/Application.js
+++ src/components/Application.js
@@ -20,6 +20,7 @@ export default function Application(props) {
   const appointments = getAppointmentsForDay(state, state.day);
   const interviewers = getInterviewersForDay(state, state.day);
 
+  
   const schedule = appointments.map((appointment) => {
     return (
       <Appointment
